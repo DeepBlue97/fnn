656e75155a602d5fbb88fd0c27659d3efe00cac2
DeepBlue97 <945048933@qq.com>
2023-12-01 11:11:04 +0800

    git pull

---
+++ submodules_pull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 定义要遍历的文件夹列表
+folders=(
+  "fnnaug"
+  "fnnconfig"
+  "fnndataset"
+  "fnnfunctor"
+  "fnnmodel"
+  "fnnmodule"
+  "fnnschedule"
+  "fnnvisual"
+)
+
+# 遍历文件夹并执行pip install -e .
+for folder in "${folders[@]}"; do
+  cd "$folder" || exit
+  git pull
+  cd ..
+done
