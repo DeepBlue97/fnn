d73d0898ef481d269defafd49f4e4f0a1a4b5ecc
wangyq <945048933@qq.com>
2023-12-20 02:44:04 +0000

    docker train ok

---
+++ docker/00_train/run.sh
@@ -0,0 +1,12 @@
+cd /workspaces
+
+# access to conda env
+source /opt/vitis_ai/conda/bin/activate vitis-ai-pytorch
+
+# install fnn submodules
+./submodules_install.sh
+
+# link soft
+ln -sf /config.py /workspaces/fnnconfig/src/fnnconfig/config__.py
+
+python -m fnnmodel.tools.run train fnnconfig.config__
