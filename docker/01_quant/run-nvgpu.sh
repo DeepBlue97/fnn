c53644514e633bb64863693226acdf594689b1cd
wangyq <945048933@qq.com>
2023-12-22 02:46:41 +0000

    multi device quant

---
+++ docker/01_quant/run-nvgpu.sh
@@ -0,0 +1,12 @@
+cd /workspaces
+
+# access to conda env for viti-ai container
+source /opt/vitis_ai/conda/bin/activate vitis-ai-pytorch
+
+# install fnn submodules
+./submodules_install.sh
+
+# link soft
+ln -sf /config.py /workspaces/fnnconfig/src/fnnconfig/config__.py
+
+python -m fnnmodel.tools.run deploy fnnconfig.config__
